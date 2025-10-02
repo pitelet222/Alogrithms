9d4c987c3505d6b4cfb5e44c88cc97b6a3b0262f
pitelet222 <pite2604@gmail.com>
2025-10-02 21:31:43 +0200

    added some notebooks to practice

---
+++ quick_sort.py
@@ -0,0 +1,11 @@
+def quicksort(arr):
+    if len(arr) < 2:
+        return arr
+    else:
+        pivot = arr[0]
+        less = [i for i in arr[1:] if i <= pivot]
+        greater = [i for i in arr[1:] if i > pivot]
+
+        return quicksort(less) + [pivot] + quicksort(greater)
+    
+print(quicksort([1,3,43,345,346,453,456,6546,645,645,36,4565,4]))
